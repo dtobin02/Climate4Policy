083237867ed289dd50ffbd8d073ed028d111075d
Ashlyn Kong <kong.ashlyn@gmail.com>
2019-01-13 18:05:49 -0500

    create D3 line chart for CA gdp vs emissions data

--- ghg-gdp/viz/viz.js
+++ ghg-gdp/viz/viz.js
@@ -1,8 +1,8 @@
-var margin = {top: 30, right: 40, bottom: 30, left: 50},
+var margin = {top: 30, right: 40, bottom: 30, left: 70},
     width = 600 - margin.left - margin.right,
     height = 270 - margin.top - margin.bottom;
 
-var parseDate = d3.time.format("%Y").parse;
+var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 var x = d3.time.scale().range([0, width]);
 var y0 = d3.scale.linear().range([height, 0]);
@@ -48,9 +48,9 @@ d3.csv("ghg-gdp-ca.csv", function(error, data) {
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.year; }));
     y0.domain([0, d3.max(data, function(d) {
-    return Math.max(d.gdp); })]); 
+        return Math.max(d.gdp); })]); 
     y1.domain([0, d3.max(data, function(d) { 
-    return Math.max(d.co2); })]);
+        return Math.max(d.co2); })]);
 
     svg.append("path")        // Add the valueline path.
         .attr("d", valueline(data));
@@ -67,12 +67,28 @@ d3.csv("ghg-gdp-ca.csv", function(error, data) {
     svg.append("g")
         .attr("class", "y axis")
         .style("fill", "steelblue")
-        .call(yAxisLeft); 
+        .call(yAxisLeft);   
 
-    svg.append("g")       
-        .attr("class", "y axis")  
-        .attr("transform", "translate(" + width + " ,0)") 
-        .style("fill", "red")   
+    svg.append("text")       // left y axis label
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("GDP per Capita");  
+
+    svg.append("g")             
+        .attr("class", "y axis")    
+        .attr("transform", "translate(" + width + " ,0)")   
+        .style("fill", "red")       
         .call(yAxisRight);
+
+    svg.append("text")       // right y axis label
+      .attr("transform", "rotate(-90)")
+      .attr("y", margin.right - 0)
+      .attr("x",(height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Carbon Dioxide Emissions");  
 
 });
